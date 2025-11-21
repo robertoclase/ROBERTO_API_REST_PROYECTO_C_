a1aaff95a4b85a21835b2b059e08531ffeabad1b
Roberto <montanes.rarob24@triana.salesianos.edu>
2025-11-21 12:08:54 +0100

    Feat: Proyecto

---
+++ src/main/java/com/salesianostriana/dam/apirestroberto/RobertoApiRestProyectoCApplication.java
@@ -0,0 +1,13 @@
+package com.salesianostriana.dam.apirestroberto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RobertoApiRestProyectoCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RobertoApiRestProyectoCApplication.class, args);
+	}
+
+}
